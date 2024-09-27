4a75efb680529d49cb537babcb0efd8ba10fbc32
wxy <1743657883@qq.com>
2024-09-27 11:57:42 +0800

    mysql主从

--- 技术文档/数据库/mariadb主从复制/catMariadbLogFile.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# 获取主从复制的log_file mysql-bin.000004
-mysql -e "show master status" |awk "NR==2"|awk '{print $1}'
--- 技术文档/数据库/mariadb主从复制/catMariadbLogPos.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# 获取主从复制的log_pos 887
-mysql -e "show master status" |awk "NR==2"|awk '{print $2}'
